2dd57463588b008a05a59001e7e786b4ca0bd705
Charlie <caam_1275@hotmail.com>
2019-05-25 14:17:05 -0500

    stash pop, no se que hice, pero borre

--- letter.js
+++ letter.js
@@ -5,28 +5,45 @@ var Letter = function (letter){
 
     this.letter = letter;
     this.guessed = false;
-    this.check = function(letra){
-        if (this.letter === letra){
+
+    this.toString = function(){
+
+        if(this.letter === " "){
             this.guessed = true;
-            this.display();
+            return " "
+        }else{
+            if(this.guessed === false){
+                return "_";                
+            }else{
+                return this.letter;
+            }
+        }
+    };
+    this.check = function(guess){
+        if (this.letter === guess){
+            this.guessed = true;
         }else{
             this.guessed = false
-            this.display();
-        };
-    };
-    this.display = function(){
-        if (this.guessed === false){
-            this.letter = "_";
-            console.log("FALSE:", this.letter)
-        }else{
-            this.letter = letra;
-            console.log("TRUE:", letter);
         };
     };
 };
 
+/* Letter.prototype.toString = function(){
+    var underscore = this.letter +' soy una *** eme';
+    return underscore;
+}
+
+var eme = new Letter("m");
+ */
+
+//console.log(eme + "");
+
+/*
+
+eme.display(); */
+
 /* === TESTING AREA === */
-var letra = new Letter("h");
+/* var letra = new Letter("h");
 
 console.log("letra", letra.letter);
 
@@ -34,5 +51,5 @@ letra.display();
 
 letra.check("h");
 letra.display();
-
+ */
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,19 +1,49 @@
 var Letter = require("./letter");
 
 var Word = function(secretWord){
-
+    //this.newArray = [];
     this.lettersArray = [];
     this.secretWord = secretWord;
-    this.displayWord = function(){
+/*     this.displayWord = function(){
         for (var i = 0; i < secretWord.length; i++) {
             this.lettersArray.push(new Letter(secretWord[i]));
             this.lettersArray[i].display(); 
         }
+    } */
+    this.pushWord = function(){
+        for (var i = 0; i < secretWord.length; i++) {
+            var letterObj = new Letter(secretWord[i]);
+            this.lettersArray.push(letterObj);
+            //console.log(this.lettersArray); 
+        };
+    };
+    this.displayWord = function(){
+        blanks = "";
+        for (var j = 0; j < this.lettersArray.length; j++) {
+             blanks += this.lettersArray[j] + " ";            
+        };
+     console.log((blanks.toUpperCase() + "plis"));   
+    };
+
+    this.checkGuess = function(guess){
+        for (var i = 0; i < this.lettersArray.length; i++) {
+            this.lettersArray[i].check(guess)
+            
+        }
     }
 
+};
 
-}
+// ==== After a lot of effort, thinking and nosebleed, these lines are for testing that the word, kinda runs! ===== 
+
+/*
 
 var palabra = new Word("hola");
 
+palabra.pushWord();
+
 palabra.displayWord();
+*/
+
+module.exports = Word;
+
